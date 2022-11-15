92f278de07e4046fda8b69e0bcd1fbd4f579ccb5
DumeGrisoni <misanthrope20218@gmail.com>
2022-11-15 09:45:00 +0100

    NavBar & Dependencies
    Modifying the NavBar component and it styles.
    Addind sanity dependencies for fetching the backend

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,13 @@ import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CS
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 import NavBar from "../components/NavBar";
+import Header from "../components/Header";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <div>
+    <div className="app-container">
       <NavBar />
+      <Header />
       <Component {...pageProps} />
     </div>
   );
