42825f2d7bc2bab54374a5a02e48a4ba14fb7b78
DumeGrisoni <misanthrope20218@gmail.com>
2022-11-15 11:02:23 +0100

    BackEnd Config
    Setting up the backend Config, fetch datas

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,28 @@
-const Home = () => {
+import groq from "groq";
+import { getClient } from "../lib/sanity.server";
+
+const Home = ({ articles }) => {
+  console.log(articles);
   return <></>;
 };
+
+export async function getStaticProps({ preview = false }) {
+  const articles = await getClient(preview).fetch(groq`
+    *[_type == "article" && publishedAt < now()] | order(publishedAt desc) {
+      _id,
+      title,
+      "username": author->author,
+      "categories": categorie[]->{id, title},
+      "authorImage": author->avatar,
+      miniature,
+      slug,
+      publishedAt
+    }`);
+  return {
+    props: {
+      articles,
+    },
+  };
+}
+
 export default Home;
