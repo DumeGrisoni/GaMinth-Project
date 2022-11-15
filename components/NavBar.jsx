a6df820681c92fe0483557a0fe3f345b75062eb9
DumeGrisoni <misanthrope20218@gmail.com>
2022-11-15 08:29:30 +0100

    First Commit
    Sass, Next.JS blog App

---
+++ components/NavBar.jsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleInfo,
+  faUserGroup,
+  faMagnifyingGlass,
+  faHouse,
+} from "@fortawesome/free-solid-svg-icons";
+
+const NavBar = () => {
+  return (
+    <nav className="nav-container">
+      <Link href={"/"} className="nav-item">
+        <FontAwesomeIcon
+          icon={faHouse}
+          style={{ height: "1.5rem", width: "1.5rem" }}
+        />
+        <span>GaMinth</span>
+      </Link>
+      <Link href={"/"} className="nav-item">
+        <FontAwesomeIcon
+          icon={faMagnifyingGlass}
+          style={{ height: "1.5rem", width: "1.5rem" }}
+        />
+        <span>Rechercher</span>
+      </Link>
+      <Link href={"/"} className="nav-item">
+        <FontAwesomeIcon
+          icon={faCircleInfo}
+          style={{ height: "1.5rem", width: "1.5rem" }}
+        />
+        <span>Infos</span>
+      </Link>
+      <Link href={"/"} className="nav-item">
+        <FontAwesomeIcon
+          icon={faUserGroup}
+          style={{ height: "1.5rem", width: "1.5rem" }}
+        />
+        <span>Social</span>
+      </Link>
+    </nav>
+  );
+};
+
+export default NavBar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.scss";
+import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+import NavBar from "../components/NavBar";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <NavBar />
+      <Component {...pageProps} />
+    </div>
+  );
+};
 
-export default MyApp
+export default MyApp;
